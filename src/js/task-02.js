e4c58397c73aa90bb7e632eb611a3a64029a4aab
Umberban <romanovborysv@gmail.com>
2022-12-21 21:43:51 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,15 +7,31 @@ const ingredients = [
   'Condiments',
 ];
 
-const containerEl = document.getElementById("ingredients");
-let ingArray = [];
-const createIng = ingredient => 
-{
-    let z = document.createElement("li");
-    z.textContent = ingredient;
-    z.classList.add("item")
-    ingArray.push(z);
-}
+// const containerEl = document.getElementById("ingredients");
+// let ingArray = [];
+// const createIng = ingredient => 
+// {
+//     let z = document.createElement("li");
+//     z.textContent = ingredient;
+//     z.classList.add("item")
+//     ingArray.push(z);
+// }
 
-ingredients.forEach(createIng)
-containerEl.append(...ingArray);
+// ingredients.forEach(createIng)
+// containerEl.append(...ingArray);
+
+function addingListElements(id,list){
+    const containerEl = document.getElementById(id);
+    let ingArray = [];
+    const createIng = el => 
+    {
+        let z = document.createElement("li");
+        z.textContent = el;
+        z.classList.add("item")
+        ingArray.push(z);
+    }
+    
+    list.forEach(createIng)
+    containerEl.append(...ingArray);
+}
+addingListElements("ingredients",ingredients);
